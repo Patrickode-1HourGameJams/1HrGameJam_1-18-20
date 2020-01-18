7e9d4970a541f2ece8e64f29fa13d380951ee455
Patrick <prm5983@rit.edu>
2020-01-18 15:34:11 -0500

    Added basic lateral movement, added gravity

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 0.1f;
+
+    void Update()
+    {
+        LateralMovement();
+    }
+
+    private void LateralMovement()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(Vector3.forward * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.Translate(-Vector3.forward * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(Vector3.right * moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(-Vector3.right * moveSpeed);
+        }
+    }
+}
