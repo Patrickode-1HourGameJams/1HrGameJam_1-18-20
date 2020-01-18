6908a6b5ac3232af8f459fac673cc739998ea11c
Patrick <prm5983@rit.edu>
2020-01-18 16:30:34 -0500

    Added ground checking for jumping

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 playerPos;
+    private Vector3 camOffset;
+
+    private void Start()
+    {
+        playerPos = player.transform.position;
+        camOffset = transform.position - playerPos;
+    }
+
+    void Update()
+    {
+        playerPos = player.transform.position;
+
+        transform.position = new Vector3(playerPos.x, playerPos.y + camOffset.y, playerPos.z + camOffset.z);
+        transform.LookAt(player.transform);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,31 +4,60 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Rigidbody rb;
+    public MeshCollider coll;
     public float moveSpeed = 0.1f;
+    public float jumpStrength = 1;
 
     void Update()
     {
-        LateralMovement();
+        GetLateralMovement();
+        GetJumpInput();
     }
 
-    private void LateralMovement()
+    private void GetLateralMovement()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            gameObject.transform.Translate(Vector3.forward * moveSpeed);
+            TurnAndMove(Vector3.forward);
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            gameObject.transform.Translate(-Vector3.forward * moveSpeed);
+            TurnAndMove(-Vector3.forward);
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            gameObject.transform.Translate(Vector3.right * moveSpeed);
+            TurnAndMove(Vector3.right);
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            gameObject.transform.Translate(-Vector3.right * moveSpeed);
+            TurnAndMove(-Vector3.right);
         }
+    }
+
+    /// <summary>
+    /// Sets the forward vector of this gameobject to a vector, then moves forward.
+    /// </summary>
+    /// <param name="dirToFace"></param>
+    private void TurnAndMove(Vector3 dirToFace)
+    {
+        transform.forward = dirToFace;
+        transform.Translate(Vector3.forward * moveSpeed);
+    }
+
+    private void GetJumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Debug.Log("Jumped");
+            rb.AddForce((transform.TransformDirection(Vector3.forward) + Vector3.up) * jumpStrength);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        //Thanks to http://answers.unity.com/answers/196395/view.html for this!
+        return Physics.Raycast(transform.position, -Vector3.up, coll.bounds.extents.y + 0.05f);
     }
 }
